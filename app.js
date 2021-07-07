9a24bc1e80005a4befeeec82364003cadb00feb1
justin000000000000 <justinpads.acc@gmail.com>
2021-07-07 16:44:16 -0500

    b

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.set('view engine', 'pug');
 app.use(express.static('public'));
